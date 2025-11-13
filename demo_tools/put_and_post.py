2bc056fc97d73674f5b5f759cf351bc9998f8d13
MariusKarlsson <mtk@inocean.no>
2025-11-13 18:56:33 +0100

    Moving weather to standalone file

--- demo_tools/put_and_post.py
+++ demo_tools/put_and_post.py
@@ -7,6 +7,6 @@ print(r.status_code, r.json())
 
 payload = {'HS':2,'TP':9,"SimLength":200}
 r = requests.post(
-    "http://127.0.0.1:8020/celery-worker/heavy-lift",
+    "http://127.0.0.1:8020/celery-worker/submit_complicated_job",
     json=payload)
 print(r.status_code, r)
